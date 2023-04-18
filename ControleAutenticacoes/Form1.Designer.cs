0e16226194a89011a1d760c74fe41391320c9e2f
victoriakamilly <victoriacamily924@gmail.com>
2023-04-18 17:26:10 -0300

    Adicionar arquivos de projeto.

---
+++ ControleAutenticacoes/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace ControleAutenticacoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbllogin = new System.Windows.Forms.Label();
+            this.lblsenha = new System.Windows.Forms.Label();
+            this.txtlogin = new System.Windows.Forms.TextBox();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.btnentrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbllogin
+            // 
+            this.lbllogin.AutoSize = true;
+            this.lbllogin.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllogin.Location = new System.Drawing.Point(238, 121);
+            this.lbllogin.Name = "lbllogin";
+            this.lbllogin.Size = new System.Drawing.Size(51, 19);
+            this.lbllogin.TabIndex = 0;
+            this.lbllogin.Text = "Login";
+            // 
+            // lblsenha
+            // 
+            this.lblsenha.AutoSize = true;
+            this.lblsenha.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsenha.Location = new System.Drawing.Point(238, 180);
+            this.lblsenha.Name = "lblsenha";
+            this.lblsenha.Size = new System.Drawing.Size(56, 19);
+            this.lblsenha.TabIndex = 1;
+            this.lblsenha.Text = "Senha";
+            this.lblsenha.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtlogin
+            // 
+            this.txtlogin.Location = new System.Drawing.Point(304, 121);
+            this.txtlogin.Name = "txtlogin";
+            this.txtlogin.Size = new System.Drawing.Size(176, 20);
+            this.txtlogin.TabIndex = 2;
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.Location = new System.Drawing.Point(304, 180);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.Size = new System.Drawing.Size(176, 20);
+            this.txtsenha.TabIndex = 3;
+            // 
+            // btnentrar
+            // 
+            this.btnentrar.Location = new System.Drawing.Point(344, 234);
+            this.btnentrar.Name = "btnentrar";
+            this.btnentrar.Size = new System.Drawing.Size(88, 38);
+            this.btnentrar.TabIndex = 5;
+            this.btnentrar.Text = "Entrar";
+            this.btnentrar.UseVisualStyleBackColor = true;
+            this.btnentrar.Click += new System.EventHandler(this.btnentrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnentrar);
+            this.Controls.Add(this.txtsenha);
+            this.Controls.Add(this.txtlogin);
+            this.Controls.Add(this.lblsenha);
+            this.Controls.Add(this.lbllogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbllogin;
+        private System.Windows.Forms.Label lblsenha;
+        private System.Windows.Forms.TextBox txtlogin;
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.Button btnentrar;
+    }
+}
+
---
+++ ControleAutenticacoes/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleAutenticacoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnentrar_Click(object sender, EventArgs e)
+        {
+            string login;
+            string senha;
+
+            login = txtlogin.Text;
+            try
+            {
+                MailAddress loginValid = new MailAddress(login);
+            }
+            catch
+            {
+                MessageBox.Show("Emsil válido");
+            }
+            senha = txtsenha.Text;
+            if (senha.Length >= 8)
+            {
+                MessageBox.Show("Senha Válida");
+            }
+            else
+            {
+                MessageBox.Show("Senha Invalida");
+            }
+            //console.writeline(login);
+
+        }
+    }
+}
